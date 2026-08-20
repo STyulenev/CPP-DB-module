7b4ee57d608d36cbb330576d48f6f4567dfa1d47
Sergey Tyulenev <tyulenev.i@yandex.ru>
2026-08-20 15:18:09 +0300

    Added examples directory

---
+++ examples/db2_example/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+#include "Database.h"
+
+void db2Test()
+{
+    using namespace app::db2;
+
+    SQLiteDB db;
+
+    if (db.open() && db.generateDB())
+    {
+        auto dao = db.getDAO<UserDTO>();
+
+        bool status = dao->insert(
+            UserDTO{
+                .name = "Mike"
+            });
+
+        if (!status)
+        {
+            throw std::runtime_error("");
+        }
+
+        auto users = dao->selectAll();
+
+        for (auto& user : users)
+        {
+            std::cout << user.id << " " << user.name << "\n";
+        }
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    db2Test();
+
+    return 0;
+}
